0bcdc94833d1e0d58fc26c76eb93158556e39d80
Aaditya-13 <patilaadi2006@gmail.com>
2026-05-06 17:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello Cloud</h1><p>Flask app deployed on Render</p>"
+    return "<h1>Hello Cloud</h1><p>Flask app deployed on Render</p><p>Assignment Completed</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
